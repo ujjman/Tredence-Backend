68bc02f36c04bbd391bece6b10eb610379d44c72
ujjwal mishra <ujjwalsini.um@gmail.com>
2025-03-27 18:05:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 # Make sure to add this BEFORE any route definitions
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
